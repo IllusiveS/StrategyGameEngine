0794d74edf427dc943f445216239cd2863cbef2d
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2017-03-18 16:27:36 +0100

    kompiluje się edytor z TemplateManagerem

---
+++ Editor/EditorTemplates/EditorTemplateManager.cpp
@@ -0,0 +1,9 @@
+//
+// Created by wysockipatryk on 3/18/17.
+//
+
+#include "EditorTemplateManager.h"
+
+EditorTemplateManager::EditorTemplateManager() : TemplateManager(){
+
+}
---
+++ Editor/EditorTemplates/EditorTemplateManager.h
@@ -0,0 +1,16 @@
+//
+// Created by wysockipatryk on 3/18/17.
+//
+
+#ifndef STRATEGYGAME_EDITORTEMPLATEMANAGER_H
+#define STRATEGYGAME_EDITORTEMPLATEMANAGER_H
+
+#include "TemplateManager/TemplateManager.h"
+
+class EditorTemplateManager : TemplateManager {
+public:
+	EditorTemplateManager();
+};
+
+
+#endif //STRATEGYGAME_EDITORTEMPLATEMANAGER_H
